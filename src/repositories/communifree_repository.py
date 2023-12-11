85e93eab625f98f660c98755b70d3dbdecbc4073
CampusGoose <angelinaact12@gmail.com>
2023-12-11 16:40:54 -0500

    test

--- src/repositories/communifree_repository.py
+++ src/repositories/communifree_repository.py
@@ -21,6 +21,18 @@ class CommunifreeRepository:
     def search_events(self, title: str):
         searched_event = event.query.filter(event.title.ilike(title)).all()
         return searched_event
+    def get_friends_by_event(self, id):
+        # Instructor.query.filter_by(last_name='Garner').all()
+        attending = (
+    db.session.query(app_user)
+    .join(participatingIn, app_user.user_id == participatingIn.user_id)
+    .join(friends, (friends.user1_id == app_user.user_id) | (friends.user2_id == app_user.user_id))
+    .filter(participatingIn.event_id == id)
+    .all()
+)
+        
+        return attending
+        
 
 # Singleton to be used in other modules
 communifree_repository_singleton = CommunifreeRepository()
