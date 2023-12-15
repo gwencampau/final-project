a6e0b95d37898ec8eea8dc952294579e0b570eac
Kiana Manning <kmanning718@gmail.com>
2023-12-14 19:37:48 -0500

    another pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+from tests.utils import reset_db
+from src.models import app_user
+from app import app
 # Test homepage
 def test_homepage(test_app):
     response = test_app.get('/')
@@ -6,7 +9,7 @@ def test_homepage(test_app):
 # Test create event
 def test_create_event(test_app):
     response = test_app.get('/create')
-    assert response.status_code == 200
+    assert response.status_code == 302
 
 # Test event search 
 def test_event_search(test_app):
@@ -55,3 +58,17 @@ def test_settings_page(test_app):
 def test_signup_page(test_app):
     response = test_app.get('/sign_up')
     assert response.status_code == 200
+
+# Test signup POST
+def test_create_user(test_app):
+    with app.app_context():
+        reset_db()
+        res = test_app.post('/sign_up_form', data={
+            'email':'abc@123.com',
+            'username': 'testing',
+            'password': 'password'
+        }, follow_redirects=True)
+        
+        assert app_user.query.first().username == 'testing'
+        assert res.status_code == 200
+       
